1f606a13b632b795c66fde8e49c6fa5cf630e9d3
Neb <75031454+Neb-RS@users.noreply.github.com>
2026-07-03 10:10:49 +0200

    Bump version to 1.1 in user script

--- PokeClicker Hatchery Egg Identifier.user.js
+++ PokeClicker Hatchery Egg Identifier.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PokeClicker Hatchery Egg Identifier
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Overlays each hatchery egg with the Pokémon name and catch/shiny status.
 // @author       Neb
 // @license      MIT
